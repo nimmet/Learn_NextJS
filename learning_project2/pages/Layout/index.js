83b1c6c1d1bd721dd7fe89269bf18fbbd17634c3
@nimmet <nimetullah@live.com>
2022-11-18 11:04:19 +0300

    new project - Event tracking app

---
+++ learning_project2/pages/Layout/index.js
@@ -0,0 +1,10 @@
+
+
+
+const Layout = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default Layout
---
+++ learning_project2/pages/index.js
@@ -0,0 +1,12 @@
+
+import styles from '../styles/Home.module.css'
+import Navbar from './Navbar'
+
+export default function Home() {
+  return (
+   <div>
+   <Navbar />
+    <h1>nextEvents</h1>
+   </div>
+  )
+}
