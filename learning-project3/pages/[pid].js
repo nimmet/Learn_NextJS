1e98e38222dd17bc8614d83fa5e55bb9dc95c48f
@nimmet <nimetullah@live.com>
2022-11-21 19:37:32 +0300

    dynamic page

---
+++ learning-project3/pages/[pid].js
@@ -0,0 +1,52 @@
+import fs from 'fs/promises'
+import path from 'path'
+import React from 'react'
+
+const ProductDetailPage = (props) => {
+
+    const {loadedProduct} = props
+
+    // if(!loadedProduct){
+    //     return <p>Loading...</p>
+    // }
+
+  return (
+    <div>
+        <h1>{loadedProduct.title}</h1>
+        <p>{loadedProduct.description}</p>
+    </div>
+  )
+}
+
+export async function getStaticProps(context) {
+const {params} = context
+
+const productId = params.pid 
+
+const filePath = path.join(process.cwd(),'data','dummy-backend.json')
+  const jsonData = await fs.readFile(filePath)
+  const data = JSON.parse(jsonData)
+
+  const product = data.products.find(product=>product.id === productId)
+
+  return {
+    props: {
+        loadedProduct: product
+    }
+  }
+
+}
+
+export async function getStaticPaths(){
+    return {
+        paths:[
+            { params: {pid:'p1'}},
+            // { params: {pid:'p2'}},
+            // { params: {pid:'p3'}},
+            // { params: {pid:'p4'}},
+        ],
+        fallback:'blocking'
+    }
+}
+export default ProductDetailPage
+
--- learning-project3/pages/index.js
+++ learning-project3/pages/index.js
@@ -1,4 +1,5 @@
 import fs from 'fs/promises'
+import Link from 'next/link';
 import path from 'path'
 
 export default function Home(props) {
@@ -8,21 +9,37 @@ export default function Home(props) {
     <div className="text-center">
       <ul>
         {products.map(product => 
-          <li key={product.id}>{product.title}</li>
+          <li key={product.id}>
+          <Link href={`/${product.id}`}>{product.title}</Link>
+          </li>
         )}
       </ul>
     </div>
   );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
+  console.log('(Re-)Generating...');
   const filePath = path.join(process.cwd(),'data','dummy-backend.json')
   const jsonData = await fs.readFile(filePath)
   const data = JSON.parse(jsonData)
+
+  if(!data){
+    return {
+      redirect: {
+      destination : '/no-data',
+    }
+  }
+  }
+
+  if(data.products.length === 0){
+    return { notFound: true}
+  }
   return {
     props: {
       products: data.products,
     },
     revalidate:120,
+    
   };
 }
