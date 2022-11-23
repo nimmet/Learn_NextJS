ff68e4bb70c2ddb714d6fdcf71dd22e2941d42ed
@nimmet <nimetullah@live.com>
2022-11-23 20:27:31 +0300

    dynamic api routing

---
+++ learning_project4/pages/feedback/index.js
@@ -0,0 +1,46 @@
+import { Fragment, useState } from "react";
+import { buildFeedbackPath, extractFeedback } from "../api/feedback";
+
+function FeedbackPage(props) {
+  const [feedbackData, setFeedbackData] = useState();
+
+  function loadFeedbackHandler(id) {
+    fetch(`/api/feedback/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setFeedbackData(data.feedback);
+      });
+  }
+
+  return (
+      <>
+      {
+        feedbackData && <p>{feedbackData.email}</p>
+      }
+    <ul>
+      {
+        props.feedbackItems.map((item) => (
+        <li key={item.id}>
+          {item.text}{'  '}
+          <button onClick={loadFeedbackHandler.bind(null, item.id)} >
+            Show Details
+          </button>
+        </li>
+      ))}
+    </ul>
+   
+  </>
+)
+      }
+
+export async function getStaticProps() {
+  const filePath = buildFeedbackPath();
+  const data = extractFeedback(filePath);
+
+  return {
+    props: {
+      feedbackItems: data,
+    },
+  };
+}
+export default FeedbackPage;
--- learning_project4/pages/index.js
+++ learning_project4/pages/index.js
@@ -1,20 +1,31 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   const emailInput = useRef()
   const feedbackInput = useRef()
+  const [feedbackItems,setFeedbackItems] = useState([])
 
   function submitFormHandler(e){
     e.preventDefault()
     const enteredEmail = emailInput.current.value
     const enteredFeedback = feedbackInput.current.value
-
-    fetch()
+    const reqBody = {email: enteredEmail, text: enteredFeedback}
+    fetch('/api/feedback',{
+      method: 'POST',
+      body: JSON.stringify(reqBody),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }).then((response)=>response.json())
+    .then((data)=> console.log(data))
   }
 
+  function loadFeedbackHandler(){
+   fetch('/api/feedback').then((response)=>response.json()).then((data)=> {setFeedbackItems(data.feedback)})
+  }
   return (
     <div>
       <h1>The Home Page</h1>
@@ -28,8 +39,18 @@ export default function Home() {
           <label htmlFor="feedback">Your Feedback </label>
           <textarea type="text" id='feedback' rows='5' ref={feedbackInput}></textarea>
         </div>
-      <button>Send Feedback</button>
+      <button onSubmit={submitFormHandler}>Send Feedback</button>
       </form>
+      <hr/>
+      <button onClick={loadFeedbackHandler}>Load Feedback</button>
+
+      <ul>
+        {
+          feedbackItems.map((item)=> (
+            <li key={item.id}>{item.text}</li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
